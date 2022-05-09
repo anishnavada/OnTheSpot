ae6c6d1884b4894405bc5ca1a37cf4a7c4b1bbd2
Anish Navada <agn36@cornell.edu>
2022-05-08 21:08:16 -0400

    add stuff

---
+++ script.js
@@ -0,0 +1,32 @@
+const title = document.getElementById("title");
+const line1 = document.getElementById("l1"); 
+const line2 = document.getElementById("l2");
+const downArrow = document.getElementById('down-arrow');
+const downArrowHover = document.getElementById('down-arrow-hover');
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+async function showText(){
+    await delay(1000);
+    title.style.opacity="1";
+    await delay(2000);
+    line1.style.opacity="1";
+    await delay(2000);
+    line2.style.opacity="1";
+    await delay(2000);
+    downArrow.style.opacity="1";
+    downArrowHover.style.opacity="1";
+}
+
+downArrow.addEventListener('mouseover', function handleMouseOver() {
+    downArrow.style.display = 'none';
+    downArrowHover.style.display = "block"; 
+    console.log("HOVER");
+});
+
+downArrowHover.addEventListener('mouseout', function handleMouseOver() {
+    downArrowHover.style.display = 'none';
+    downArrow.style.display = "block"; 
+});
